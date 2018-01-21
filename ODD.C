2a51ba6dc466f5c7415a5974720aa27bf22a414f
PRAVEENKUMARSPKMECH <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-01-21 15:05:02 +0530

    Create ODD.C

---
+++ ODD.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;clrscr();
+printf("\n ENTER THE VALUE OF n");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n IT IS EVEN NUMBER");
+else
+printf("\n IT IS ODD NUMBER");
+getch();
+}
