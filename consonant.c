76dd835f1e596800376774f79d2a14906f63e6a6
PRAVEENKUMARSPKMECH <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-01-21 16:21:26 +0530

    Rename const.c to consonant.c

---
+++ consonant.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char x;
+clrscr();
+printf("\n ENTER THE LETTER ");
+scanf("%c",&x);
+if(x=='A')
+{
+printf("\n IT IS A VOWEL");
+}
+else if(x=='a')
+{
+
+printf("\n IT IS A VOWEL");}
+else if(x=='E')
+{
+
+printf("\n IT IS A VOWEL");}
+
+else if(x=='e')
+{
+
+printf("\n IT IS A VOWEL");}
+else if(x=='i')
+{
+
+printf("\n IT IS A VOWEL");}
+ else if(x=='I')
+ {
+
+printf("\n IT IS A VOWEL");}
+ else if(x=='O')
+
+{
+
+printf("\n IT IS A VOWEL");}
+
+else if(x=='o')
+{
+
+printf("\n IT IS A VOWEL");}
+else if(x=='u')
+{
+
+printf("\n IT IS A VOWEL");}
+else if(x=='U')
+{
+
+printf("\n IT IS A VOWEL");
+}
+else
+{
+
+printf("\n IT IS THE CONSONANT");}
+getch();
+
+}
