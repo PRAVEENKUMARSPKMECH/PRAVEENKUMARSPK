6deaee29b0273948329a1f8c4d1d58c743faa720
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-01-30 16:16:26 +0530

    Update EVENLIM.C

--- EVENLIM.C
+++ EVENLIM.C
@@ -8,7 +8,7 @@ void main()
    printf("ENTER THE LIMITS ");
    scanf("%d%d",&a,&b);
       printf("THE EVEN NUMBERS BETWEEN %d AND %d :",a,b);
-   for(i=a;i<=b;i++)
+   for(i=a;i<b;i++)
 {
     if(i%2==0)
     {
