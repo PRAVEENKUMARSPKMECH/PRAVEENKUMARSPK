e7ae4a6e4e43127d1bdaa108378b63f4c960a172
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-02-06 16:46:38 +0530

    Create mf.c

---
+++ mf.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+  #include<conio.h>
+void main()
+{
+    int a,b,c,k,i;clrscr(); 
+    printf("\nENTER THE NUMBER");
+    scanf("%d",&a);
+
+    for(i=1;i<=5;i++)
+    {
+  k=a*i;
+  printf("\n%d",k);
+    }
+ 
+getch();
+ 
+}
