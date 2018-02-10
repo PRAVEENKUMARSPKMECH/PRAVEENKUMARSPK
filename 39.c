3087f570c985a9410c0f6807dced42e83925b8f0
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-02-10 12:08:59 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main() 
+
+{
+   
+int a[1000],n,j,i;clrscr();
+  
+ 
+   printf("\nENTER THE TIME IN MINUTES");
+
+   scanf("%d",&n);
+    
+  j=n/60;
+ 
+     i=n%60;
+    
+ 
+    {
+     printf("\n THE TIME IS %d hour and %d minutes",j,i); 
+      }
+      
+
+        
+    
+    getch();
+}
