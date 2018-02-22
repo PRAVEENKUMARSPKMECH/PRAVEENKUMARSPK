f1b1e2e0f332a0d49b53cb933907f3c2a681c8c2
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-02-22 16:46:22 +0530

    Create 411.c

---
+++ 411.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include<string.h>   
+int main()
+{
+int i,n;
+char s[10];
+printf("Enter the string");
+scanf("%s",&s);
+printf("enter the count ");
+scanf("%d",&n);
+for (i=1;i<=n;i++)
+    {
+printf("\n%s",s);
+    }
+    return 0;
+}
