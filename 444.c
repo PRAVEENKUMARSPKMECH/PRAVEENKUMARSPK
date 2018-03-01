48e2036fbe56401b74a97e238134366ea2aecf3f
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-03-01 16:29:09 +0530

    Create 444.c

---
+++ 444.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int i;clrscr();
+printf("enter the number");
+scanf("%d",&i);
+
+if((i>1)&&(i<9)&&(i>0))
+printf("\n yes");
+else
+printf("\n no");
+
+getch();
+}
