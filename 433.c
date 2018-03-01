70c5166f827ac0696583c2ce566739c9b0fd6e0e
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-03-01 16:17:50 +0530

    Create 433.c

---
+++ 433.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+char a[20],b[50];clrscr();
+int i;
+printf("\nEnter string1");
+scanf("%s",&a);
+printf("\nEnter string2");
+scanf("%s",&b);
+printf("\n%s%s",a,b);
+getch();
+}
