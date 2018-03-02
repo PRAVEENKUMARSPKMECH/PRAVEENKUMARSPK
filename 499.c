3ae268bc63236c56389b3deb8684c5a4ce54d911
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-03-02 14:10:41 +0530

    Create 499.c

---
+++ 499.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int n,v,s;clrscr();
+   printf("ENTER TWO NUMBERS ");
+   scanf("%d%d",&n,&v);
+  s=n+v;
+
+    printf("\n SUM = %d",s);
+    getch();
+}
