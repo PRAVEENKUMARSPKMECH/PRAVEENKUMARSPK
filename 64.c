1be9ce83623d76655bbb4c1b2666f5b310a56ac8
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-03-15 16:26:33 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+
+{
+    	int a,b,x;
+
+printf("ENTER TWO NUMBERS");
+	scanf("%d%d",&a,&b);
+x=a+b;
+if(x%2==0)
+printf("\nSUM OF THEASE 2 NUMBERS IS AN EVEN NUMBER");
+else
+printf("\nSUM OF THEASE 2 NUMBERS IS AN ODD NUMBER");
+
+}
