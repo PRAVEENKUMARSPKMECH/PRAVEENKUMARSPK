886fe8dd425677b3f4505750ee4ae8760498840a
PRAVEEN KUMAR SPK <35652440+PRAVEENKUMARSPKMECH@users.noreply.github.com>
2018-03-20 16:05:14 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+int num,i,flag=0;
+printf("\n ENTER a NUMBER");
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+if(num%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("\n it is a prime number");
+}
+else
+{
+printf("\n it is not a prime number");
+}
+return 0;
+
+}
